eb7d855fe0fa3c139df7b232507df30971c283c5
Sohrab Khan <122888702+sohrabmvsol@users.noreply.github.com>
2024-06-12 12:22:45 +0500

    added initial guess generator and dynamic colors

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
-const GameScreen = () => {
+function generateRandomBetween(min, max, exclude) {
+	const rndNum = Math.floor(Math.random() * (max - min)) + min;
+	if (rndNum == exclude) {
+		return generateRandomBetween(min, max, exclude);
+	} else {
+		return rndNum;
+	}
+}
+const GameScreen = ({ userGuess }) => {
+	const initialGuess = generateRandomBetween(1, 100, userGuess);
+	const [currentGuess, setCurrentGuess] = useState(initialGuess);
 	return (
 		<View style={styles.screen}>
-			<Text style={styles.title}>Opponent's Guess</Text>
+			<Title>Opponent's Guess</Title>
 			<View>
 				<Text>Higher Or Lower</Text>
 			</View>
@@ -14,15 +25,6 @@ const GameScreen = () => {
 
 const styles = StyleSheet.create({
 	screen: { flex: 1, padding: 24 },
-	title: {
-		fontSize: 24,
-		fontWeight: "bold",
-		color: "#ddb52f",
-		textAlign: "center",
-		borderWidth: 2,
-		borderColor: "#ddb52f",
-		padding: 12,
-	},
 });
 
 export default GameScreen;
